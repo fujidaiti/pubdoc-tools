0811bd5af57ac09ea0b5930e4e0b4f1052005cf7
fujidaiti <fujidaiti@gmail.com>
2026-03-23 01:03:54 +0900

    misc

--- pubdoc/lib/src/environment.dart
+++ pubdoc/lib/src/environment.dart
@@ -21,5 +21,5 @@ class PlatformEnvironment implements Environment {
   String? getVariable(String name) => Platform.environment[name];
 
   @override
-  String get toolVersion => '0.0.1';
+  String get toolVersion => '0.1.0';
 }
